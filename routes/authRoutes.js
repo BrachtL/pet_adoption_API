d43dbb14406d3e81ce497ceb02f33eb8135aa196
Luciano Bracht <lucianoeletronico@gmail.com>
2023-06-25 22:04:28 -0300

    CREATE: authMiddleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const authController = require('../controllers/authController');
+const { requireAuth } = require('../middleware/authMiddleware');
 
 const router = Router();
 
 router.post('/signup', authController.signup_post);
 router.post('/login', authController.login_post);
+router.post('/tokenTest', requireAuth, (req, res) => {res.send("All Right")});
+//this method should be get, and token should be sent in header
 
 const teste = process.env.BFT_DB_HOST;
 module.exports = {router, teste }
