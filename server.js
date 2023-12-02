7ae456540a0d92fb3558a5b3d9dd07b24c846272
Luciano Bracht <lucianoeletronico@gmail.com>
2023-12-02 08:38:50 -0300

    FEATURE: created sockets to chat, but it is not configured for a private chat between 2 users yet

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 const express = require("express");
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
 const app = express();
+const mainServer = http.createServer(app);
+const io = socketIo(mainServer);
 
-require('dotenv').config()
 
-//middleware (ponte entre requests)
+
+//this HTML is just for testing the sockets
+// Serve the HTML file at /socket
+app.get('/socket', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+// Serve Socket.IO library using express.static
+app.use('/socket.io', express.static(path.join(__dirname, 'node_modules/socket.io-client/dist')));
+
+// Handle socket connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle incoming messages
+  socket.on('chat message', (msg) => {
+    console.log(`Message from user: ${msg}`);
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+
+
+// Main Express server
 app.use(express.json());
-
 const routes = require('./routes/routes').router
 app.use(routes);
 
@@ -24,29 +57,22 @@ process.on('unhandledRejection', (err) => {
   process.exit(1);
 });
 
-
-
-//check if server is online
+// Check if the main server is online
 app.route("/").get((requirement, response) => {
-  response.send("The server is online");
-  console.log("The server is online");
+  response.send("The main server is online");
+  console.log("The main server is online");
 });
 
-app.listen(3000);
-console.log("successful loaded :D");
+// Start the main server on port 3000
+const PORT_MAIN = 3000;
+mainServer.listen(PORT_MAIN, () => {
+  console.log(`Main server is running on http://localhost:${PORT_MAIN}`);
+});
 
 
 
+// Start the Socket.IO server on port 3500
+const PORT_SOCKETIO = 3500;
+io.listen(PORT_SOCKETIO);
+console.log(`Socket.IO server is running on http://localhost:${PORT_SOCKETIO}`);
 
-/*
-  db table -> test_user
-  db fields -> id (AI, PK), email (UNIQUE), password (char(60))
-  next steps:
-  - create the signup and sign in, with hash and jwt
-  - test with insomnia
-  
-  - model db, user data
-  - begin app dev
-  - backend dev
-  - test
-*/
